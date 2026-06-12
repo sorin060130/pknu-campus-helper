a8cd304ba6fd78b1832e8c5b423735979721853e
sorin060130 <seongil@pukyong.ac.kr>
2026-06-12 19:29:30 +0900

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// 将 Firebase 控制台 > 项目设置 > 您的应用 > SDK 设置中的值粘贴到这里。
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
